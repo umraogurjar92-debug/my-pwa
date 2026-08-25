ea667e4f1883c278734537309df57903ea391b6e
umraogurjar92-debug <umraogurjar92@gmail.com>
2026-08-25 14:59:14 +0530

    Add PainMed Pro Android WebView launcher

---
+++ android/app/src/main/java/com/painmedpro/app/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.painmedpro.app
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        webView = WebView(this)
+        setContentView(webView)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = false
+        webView.settings.allowContentAccess = false
+        webView.settings.userAgentString = webView.settings.userAgentString + " PainMedProAndroid/1.0"
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                val url = request.url.toString()
+                return if (url.startsWith("https://my-pwa-indol.vercel.app")) { false } else {
+                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url))); true
+                }
+            }
+        }
+        webView.loadUrl("https://my-pwa-indol.vercel.app/")
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (webView.canGoBack()) webView.goBack() else super.onBackPressed()
+    }
+}
